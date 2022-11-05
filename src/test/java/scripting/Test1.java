7ac3216c0426ade6e5409b85791b2cf94b6e0335
HOME <HOME@DESKTOP-J9RPTM9>
2022-11-05 12:28:11 +0530

    hi

---
+++ src/test/java/scripting/Test1.java
@@ -0,0 +1,14 @@
+package scripting;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void tc1()
+	{
+		Reporter.log("hello    java" ,true);
+	}
+
+}
---
+++ src/test/java/scripting/Test3.java
@@ -0,0 +1,15 @@
+package scripting;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	
+	@Test
+	
+	public void tc3()
+	{
+		Reporter.log("Hello python",true);
+	}
+
+}
